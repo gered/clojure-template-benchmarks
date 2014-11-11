e245fe7bf1f1c83d4e57d12b3fd962de6df47b98
gered <gered@blarg.ca>
2014-11-11 15:15:59 -0500

    some minor cleanup

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to clojure-template-benchmarks
-
-TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- test/clojure_template_benchmarks/core_test.clj
+++
@@ -1,8 +0,0 @@
-(ns clojure-template-benchmarks.core-test
-  (:use clojure.test
-        clojure-template-benchmarks.core))
-
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
